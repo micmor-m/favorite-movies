9d91c808970c06560033a45207d2fde98b84a72c
mic <micmor.m@gmail.com>
2020-11-20 22:05:40 -0500

    clear input field when pressing Cancel or Add

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,10 @@ const startAddMovieButton = document.querySelector("header button");
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
 const backdrop = document.getElementById("backdrop");
 const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
-console.log(cancelAddMovieButton);
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+// const userInput = addMovieModal.getElementsByTagName("input");
+const userInputs = addMovieModal.querySelectorAll("input");
+const movies = [];
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -16,8 +19,44 @@ const toggleMovieModal = () => {
   toggleBackdrop();
 };
 
-const cancelAddMovie = () => {
+const clearMovieInput = () => {
+  for (const userInput of userInputs) {
+    userInput.value = null;
+  }
+};
+
+const cancelAddMovieHandler = () => {
   toggleMovieModal();
+  clearMovieInput();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("Please enter valid values (rating between 1 and 5).");
+    return;
+  }
+
+  newMovies = {
+    title: titleValue,
+    imageUrl: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovies);
+
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInput();
 };
 
 const backdropClickHandler = () => {
@@ -26,4 +65,5 @@ const backdropClickHandler = () => {
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
 backdrop.addEventListener("click", backdropClickHandler);
-cancelAddMovieButton.addEventListener("click", cancelAddMovie);
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
